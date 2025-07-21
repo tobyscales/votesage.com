37943d34eb3a25517426e566fe3fcefdb16fda0e
Toby Scales <17342265+tobyscales@users.noreply.github.com>
2025-07-21 00:52:43 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html', // Scans any .html files in your root directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
